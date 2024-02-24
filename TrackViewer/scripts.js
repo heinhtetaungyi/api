f87768a78534d6cab5a56d525514ae4c4c3d1daf
Hein Htet Aung <142594340+heinhtetaungyi@users.noreply.github.com>
2024-02-25 00:26:15 +0630

    Update scripts.js

--- TrackViewer/scripts.js
+++ TrackViewer/scripts.js
@@ -1,5 +1,5 @@
 // Assign
-let itrack = {
+let ITRACK = {
   endKey: "",
   endPoint: "https://api.github.com/repos/heinhtetaungyi/api/contents/TrackViewer/counts.json",
   endUser: "",
@@ -7,7 +7,8 @@ let itrack = {
   char2: "TFhCSGFmNDRWaHoxZG9kYmZ6TD",
   char3: "JhZ05paDFPOG1mcQ=="
 }
-
+let ITRACK_IS_VIEWED = "itrack_isviewed";
+let ITRACK_IS_SETUP_MODE = false;
 function generateiTrackId() {
   const timestamp = Date.now().toString(36);
   const randomPart = Math.random().toString(36).substr(2,
@@ -21,15 +22,15 @@ function iTrackBtoA(str) {
   return btoa(unescape(encodeURIComponent(str)));
 }
 async function iTrackViewers() {
-  fetch(itrack.endPoint,
+  fetch(ITRACK.endPoint,
     {
       headers: {
-        Authorization: `token ${iTrackAtoB(itrack.char1+itrack.char2+itrack.char3)}`
+        Authorization: `token ${iTrackAtoB(ITRACK.char1+ITRACK.char2+ITRACK.char3)}`
       }
     })
   .then(response => response.json())
   .then(data => {
-    itrack.endKey = data.sha;
+    ITRACK.endKey = data.sha;
     let jsonData = [];
     try {
       jsonData = JSON.parse(iTrackAtoB(data.content));
@@ -51,10 +52,10 @@ async function iTrackViewers() {
 }
 async function getViewers() {
 
-  let res = await fetch(itrack.endPoint,
+  let res = await fetch(ITRACK.endPoint,
     {
       headers: {
-        Authorization: `token ${iTrackAtoB(itrack.char1+itrack.char2+itrack.char3)}`
+        Authorization: `token ${iTrackAtoB(ITRACK.char1+ITRACK.char2+ITRACK.char3)}`
       }
     });
   if (res.ok) {
@@ -69,15 +70,15 @@ async function getViewers() {
 }
 
 function addToViewer(viewer) {
-  fetch(itrack.endPoint,
+  fetch(ITRACK.endPoint,
     {
       headers: {
-        Authorization: `Bearer ${iTrackAtoB(itrack.char1+itrack.char2+itrack.char3)}`
+        Authorization: `Bearer ${iTrackAtoB(ITRACK.char1+ITRACK.char2+ITRACK.char3)}`
       }
     })
   .then(response => response.json())
   .then(existingData => {
-    itrack.endKey = existingData.sha;
+    ITRACK.endKey = existingData.sha;
     let jsonData = [];
     try {
       jsonData = JSON.parse(iTrackAtoB(existingData.content));
@@ -101,11 +102,11 @@ function updateiTrackList(key, array) {
     null,
     2);
   data = iTrackBtoA(data);
-  fetch(itrack.endPoint,
+  fetch(ITRACK.endPoint,
     {
       method: "PUT",
       headers: {
-        Authorization: `Bearer ${iTrackAtoB(itrack.char1+itrack.char2+itrack.char3)}`,
+        Authorization: `Bearer ${iTrackAtoB(ITRACK.char1+ITRACK.char2+ITRACK.char3)}`,
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
@@ -134,7 +135,8 @@ function updateiTrackList(key, array) {
     console.error("realtime fetch error:", error);
   });
 }
-function getDeviceType (userAgent) {
+function getDeviceType () {
+  let userAgent = navigator.userAgent;
   let deviceType;
   let tabletPattern = / (tablet|ipad|playbook|silk)| (android (?!.*mobi))/i;
   let mobilePattern = / (mobi|phone|blackberry|opera mini|fennec|minimo|symbian|psp|nintendo ds)/i;
@@ -147,11 +149,21 @@ function getDeviceType (userAgent) {
   }
   return deviceType;
 }
-function getLanguageName(code) {
+function getLanguage() {
+  let l = navigator.language;
   const languageNames = new Intl.DisplayNames("en", {
     type: "language"
   });
-  return languageNames.of(code);
+  return languageNames.of(l);
+}
+function getRegion() {
+  let l = navigator.language;
+  const locale = new Intl.Locale(l);
+  return locale.region;
+}
+function getDate() {
+  let date = new Date();
+  return date.toString();
 }
 async function startTrack() {
   let ipList = await fetch("https://api.ipify.org?format=json");
@@ -163,14 +175,15 @@ async function startTrack() {
     throw new Error("Request failed: " + ipList.status);
   }
 
-  let screenSize = window.screen.width + "x" + window.screen.height;
   let browserName = navigator.appName;
-  let platform = navigator.platform;
-  let language = navigator.language;
-  language = getLanguageName(language);
-  let onlineStatus = navigator.onLine;
+  let screenSize = window.screen.width + "x" + window.screen.height;
+  let deviceType = getDeviceType();
   let device = navigator.userAgent;
-  let deviceType = getDeviceType(device);
+  let platform = navigator.platform;
+  let language = getLanguage();
+  let region = getRegion();
+  let onlineStatus = navigator.onLine;
+  let date = getDate();
   let key = generateiTrackId();
 
   let viewer = {
@@ -179,16 +192,88 @@ async function startTrack() {
     "browserName": browserName,
     "screenSize": screenSize,
     "deviceType": deviceType,
+    "device": device,
     "platform": platform,
     "language": language,
-    "device": device
+    "region": region,
+    "date": date
   };
+  console.log(viewer);
   addToViewer(viewer);
+  //setViewed();
 }
-async function testTrack() {
+
+function isViewed() {
+  let viewed = localStorage.getItem(ITRACK_IS_VIEWED);
+  if (viewed) {
+    return true;
+  }
+  return false;
+}
+function setViewed() {
+  localStorage.setItem(ITRACK_IS_VIEWED, true);
+}
+function setTrackMode(s) {
+  let mode = s.toString().toLowerCase().trim();
+  if (mode === "1" || mode === "one" || mode === "once") {
+    // to do
+  }
+}
+function setUpToken(s) {
+  let char1 = s.substr(0, 10);
+  let char2 = s.substr(10, 10);
+  let char3 = s.substr(20, s.length);
+  ITRACK.char1 = char1;
+  ITRACK.char2 = char2;
+  ITRACK.char3 = char3;
+}
+function setUpPath(s) {
+  ITRACK.endPoint = s;
+}
+function setiTrack(s) {
+  if (s === undefined) {
+    console.log("auto");
+    startTrack();
+  } else {
+    console.log("setup");
+    ITRACK.endPoint = s;
+    startTrack();
+  }
+}
+
+async function testiTrack() {
   let v = await getViewers();
   console.log(v);
   console.log(v.length+ " Views");
 }
-startTrack();
-//testTrack();
+
+setiTrack();
+//setTrackMode(1);
+testiTrack();
+
+function timeSince(date) {
+  var seconds = Math.floor((new Date() - date) / 1000);
+  var interval = seconds / 31536000;
+  if (interval > 1) {
+    return Math.floor(interval) + " years";
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    return Math.floor(interval) + " months";
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    return Math.floor(interval) + " days";
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    return Math.floor(interval) + " hours";
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return Math.floor(interval) + " minutes";
+  }
+  return Math.floor(seconds) + " seconds";
+}
+//var aDay = 24*60*60*1000;
+//console.log(timeSince(new Date(Date.now()-aDay)));
